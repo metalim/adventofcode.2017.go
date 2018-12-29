0917b1d54cee29a595da34ac25ce5569a7578e02
Maxim Litvinov <metalim@gmail.com>
2018-12-29 18:09:18 +0200

    2017.13: calculate scanner positions without moving.

--- 13_move_through_scanners.go
+++ 13_move_through_scanners.go
@@ -2,9 +2,7 @@ package main
 
 import (
 	"fmt"
-	"metalim/advent/2017/lib/debug"
 	"metalim/advent/2017/lib/source"
-	"time"
 
 	. "github.com/logrusorgru/aurora"
 )
@@ -13,25 +11,8 @@ var test1 = `0: 3
 1: 2
 4: 4
 6: 4`
-var test2 = ``
-
-type layer struct {
-	x, y, d, l int
-}
-
-func firewall(ssn [][]int) (map[int]*layer, int) {
-	fw := map[int]*layer{}
-	var max int
-	for _, l := range ssn {
-		fw[l[0]] = &layer{x: l[0], l: l[1], d: 1}
-		max = l[0]
-	}
-	return fw, max
-}
 
 func main() {
-	// source.Dry()
-
 	var ins source.Inputs
 
 	ins = ins.Test(1, test1, `24`)
@@ -43,61 +24,39 @@ func main() {
 		ssn := p.Lines().Ints()
 		fmt.Println(len(ssn), Black(ssn[:3]).Bold())
 
+		type layer struct {
+			x, l int
+		}
+		fw := map[int]layer{}
+		for _, l := range ssn {
+			fw[l[0]] = layer{x: l[0], l: l[1]}
+		}
+
 		if p.Part(1) {
 			var sev int
-			fw, max := firewall(ssn)
 
-			for x := 0; x <= max; x++ {
-				if s, ok := fw[x]; ok {
-					if s.y == 0 { // cought?
-						sev += s.x * s.l
-					}
-				}
-				for _, s := range fw { // move scanners
-					if s.y+s.d < 0 || s.y+s.d >= s.l {
-						s.d = -s.d
-					}
-					s.y += s.d
+			// instead of moving scanners, calculate their position when packet is passing.
+			for _, s := range fw {
+				if s.x%(2*s.l-2) == 0 {
+					sev += s.x * s.l
 				}
 			}
 			p.SubmitInt(1, sev)
 		}
 
 		if p.Part(2) {
-			fw, max := firewall(ssn)
 			var x0 int
 
-		NEXT:
-			for x0 = 0; ; x0++ {
-				debug.LogT(time.Second, Black(x0).Bold())
+		DELAY:
+			for x0 = 0; ; x0++ { // try delays, until solution is found.
 
-				// pre-move scanners.
+				// instead of moving scanners, calculate their position when packet is passing.
 				for _, s := range fw {
-					mod := (s.l - 1) * 2
-					s.y = x0 % mod
-					s.d = 1
-					if s.y >= s.l {
-						s.y = mod - s.y
-						s.d = -1
+					if (x0+s.x)%(2*s.l-2) == 0 {
+						continue DELAY
 					}
 				}
-
-				for x := 0; x <= max; x++ {
-					if x >= 0 {
-						if s, ok := fw[x]; ok {
-							if s.y == 0 { // cought?
-								continue NEXT
-							}
-						}
-					}
-					for _, s := range fw { // move scanners
-						if s.y+s.d < 0 || s.y+s.d >= s.l {
-							s.d = -s.d
-						}
-						s.y += s.d
-					}
-				}
-				break // found it.
+				break // found it
 			}
 			p.SubmitInt(2, x0)
 		}
